e7e779603c40d85311fbc795318de225be111da9
Alexander Kladov <alexander.a.kladov@gmail.com>
2025-04-24 02:55:36 +0400

    Buttons pressed

--- robotarm_hanoi.py
+++ robotarm_hanoi.py
@@ -60,28 +60,34 @@ class SliderApp(QWidget):
         self.left_hanoi_pb = QPushButton("left")
         self.center_hanoi_pb = QPushButton("center")
         self.right_hanoi_pb = QPushButton("right")
-        self.claw_pb = QPushButton("claw")
+        self.claw_hanoi_pb = QPushButton("claw")
         layout_buttons.addWidget(self.left_hanoi_pb)
         layout_buttons.addWidget(self.center_hanoi_pb)
         layout_buttons.addWidget(self.right_hanoi_pb)
-        layout_buttons.addWidget(self.claw_pb)
+        layout_buttons.addWidget(self.claw_hanoi_pb)
         self.slider_hanoi_vertical = QSlider(Qt.Horizontal, self)
-        self.slider_hanoi_vertical.valueChanged.connect(self.update_label_hanoi_vertical)
 
-        self.slider_hanoi_vertical.setMaximum(180)
-        self.slider_hanoi_vertical.setValue(90)
+        self.slider_hanoi_vertical.setMaximum(5)
+        self.slider_hanoi_vertical.setValue(2)
         layout_vertical.addWidget(self.slider_hanoi_vertical)
         self.label_hanoi_vertical = QLabel("0", self)
         layout_vertical.addWidget(self.label_hanoi_vertical)
         layout_v.addLayout(layout_buttons)
         layout_v.addLayout(layout_vertical)
         layoutv.addLayout(layout_v)
+        self.update_label_hanoi_vertical()
 
         # Connect the slider's valueChanged signal to the update_label slot
         self.slider_claw.valueChanged.connect(self.update_label_claw)
         self.slider_rotate.valueChanged.connect(self.update_label_rotate)
         self.slider_fb.valueChanged.connect(self.update_label_fb)
         self.slider_ud.valueChanged.connect(self.update_label_ud)
+        self.slider_hanoi_vertical.valueChanged.connect(self.update_label_hanoi_vertical)
+
+        self.left_hanoi_pb.pressed.connect(self.left_hanoi_pressed)
+        self.center_hanoi_pb.pressed.connect(self.center_hanoi_pressed)
+        self.right_hanoi_pb.pressed.connect(self.right_hanoi_pressed)
+        self.claw_hanoi_pb.pressed.connect(self.claw_hanoi_pressed)
 
         self.setLayout(layoutv)
 
@@ -108,7 +114,19 @@ class SliderApp(QWidget):
     def update_label_hanoi_vertical(self):
         value = self.slider_hanoi_vertical.value()
         self.label_hanoi_vertical.setText(f"{value}")
-        
+
+    def left_hanoi_pressed(self):
+        print("left")
+
+    def center_hanoi_pressed(self):
+        print("center")
+
+    def right_hanoi_pressed(self):
+        print("right")
+    
+    def claw_hanoi_pressed(self):
+        print("claw")
+
     def write_to_arduino(self, command):
         print(command)
         #self.to_arduino.write(command)
